3a6b9b63fef94b60ee55ce3d238cbfc15f558487
Hasiru <bgh.kavishan@gmail.com>
2018-08-22 10:36:50 +0530

    Add index,config,and newAuthoer php files

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hasiru Kavishan
+ * Date: 8/22/2018
+ * Time: 10:31 AM
+ */
+    //create the database connection
+    $db = mysqli_connect('localhost','root','','author');
+
+?>
---
+++ newAuthor.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hasiru Kavishan
+ * Date: 8/22/2018
+ * Time: 10:31 AM
+ */
