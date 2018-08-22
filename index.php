59e52f1d887a47cfeeb30d7462cf0061800fed11
Hasiru <bgh.kavishan@gmail.com>
2018-08-22 10:54:35 +0530

    Front end created

--- index.php
+++ index.php
@@ -5,3 +5,58 @@
  * Date: 8/22/2018
  * Time: 10:30 AM
  */
+?>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Note Application</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+
+    <link rel="stylesheet" href="https://formden.com/static/cdn/font-awesome/4.4.0/css/font-awesome.min.css" />
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+
+</head>
+<body>
+<div class="container">
+    <div class="py-5 text-center">
+        <h2>Book List View</h2>
+        <p class="lead">Following are the book list created recently</p>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <table class="table table-striped py-5">
+                <thead>
+                <tr>
+                    <th scope="col">Book Title</th>
+                    <th scope="col">Author</th>
+                    <th scope="col">Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Meeting with saara</td>
+                    <td>meeeitng with saars description goes here</td>
+                    <td><button type="button" class="btn btn-success btn-sm mr-1">Edit</button><button type="button" class="btn btn-danger btn-sm">Delete</button></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="row text-right">
+        <div class="col-md-10">
+            <button type="button" class="btn btn-primary">Add New Note</button>
+        </div>
+    </div>
+
+
+
+</div>
+</body>
+</html>
--- newAuthor.php
+++ newAuthor.php
@@ -5,3 +5,54 @@
  * Date: 8/22/2018
  * Time: 10:31 AM
  */
+?>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Note Application</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+
+    <link rel="stylesheet" href="https://formden.com/static/cdn/font-awesome/4.4.0/css/font-awesome.min.css" />
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+
+</head>
+<body>
+<div class="container">
+    <div class="py-5 text-center">
+        <h2>Create New Book Here</h2>
+        <p class="lead">Please fill below details to create new book item</p>
+    </div>
+
+    <div class="row justify-content-center">
+        <div class="col-md-8 order-md-1">
+            <form>
+                <div class="mb-3">
+                    <label class="form-control-label">Book Title</label>
+                    <input class="form-control" type="text">
+                </div>
+                <div class="mb-3">
+                    <label class="form-control-label">Author</label>
+                    <input class="form-control datepicker" type="text" >
+                </div>
+                <div class="mb-3">
+                    <button type="button" class="btn btn-primary">Save New Note</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<script>
+    $('.datepicker').datepicker({
+        autoclose: "true"
+    });
+</script>
+</body>
+</html>
+
